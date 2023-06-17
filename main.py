bc7c33c4683dc2456bacdd78eefa597ec6a2a76a
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2023-06-17 17:02:42 -0300

    serial complete (I guess)

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import sudoku
+import sys
 
 def checar(tabuleiro):
-    linhas = ["L" + linha for linha in sudoku.checar_linhas(tabuleiro)]
-    colunas = ["C" + coluna for coluna in sudoku.checar_colunas(tabuleiro)]
-    regioes = ["R" + regiao for regiao in sudoku.checar_regioes(tabuleiro)]
+    linhas = ["L" + str(linha) for linha in sudoku.checar_linhas(tabuleiro)]
+    colunas = ["C" + str(coluna) for coluna in sudoku.checar_colunas(tabuleiro)]
+    regioes = ["R" + str(regiao) for regiao in sudoku.checar_regioes(tabuleiro)]
     erros = linhas + colunas + regioes
     print(f'{len(erros)} erros encontrados', end=" ")
     if len(erros) > 0:
         print(f"({', '.join(erros)})")
+    else:
+        print("")
 
-def main(arquivo):
-    tabuleiros = sudoku.ler(arquivo)
-    for t in tabuleiros:
-        checar(t)
+def main():
+    arquivo = "input-sample.txt"
+    try:
+        tabuleiros = sudoku.ler(arquivo)
+        for t in tabuleiros:
+            checar(t)
+    except:
+        print("Erro!")
+
+main()
