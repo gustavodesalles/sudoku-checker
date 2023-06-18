58f6f03035b3b9a78cc87bbd8ae34d3c25e9612d
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2023-06-17 22:45:35 -0300

    begin multithreading (only one process)

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import sudoku
 import sys
+from collections import OrderedDict
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
-def checar(tabuleiro):
-    linhas = ["L" + str(linha) for linha in sudoku.checar_linhas(tabuleiro)]
-    colunas = ["C" + str(coluna) for coluna in sudoku.checar_colunas(tabuleiro)]
-    regioes = ["R" + str(regiao) for regiao in sudoku.checar_regioes(tabuleiro)]
-    erros = linhas + colunas + regioes
-    print(f'{len(erros)} erros encontrados', end=" ")
-    if len(erros) > 0:
-        print(f"({', '.join(erros)})")
+# def checar(tabuleiro):
+#     linhas = ["L" + str(linha) for linha in sudoku.checar_linhas(tabuleiro)]
+#     colunas = ["C" + str(coluna) for coluna in sudoku.checar_colunas(tabuleiro)]
+#     regioes = ["R" + str(regiao) for regiao in sudoku.checar_regioes(tabuleiro)]
+#     erros = linhas + colunas + regioes
+#     print(f'{len(erros)} erros encontrados', end=" ")
+#     if len(erros) > 0:
+#         print(f"({', '.join(erros)})")
+#     else:
+#         print("")
+
+def checar(tabuleiro, n_threads):
+    erros = OrderedDict()
+    transposto = sudoku.transpor(tabuleiro)
+    tab_em_blocos = sudoku.obter_regioes_como_linhas(tabuleiro)
+
+    def checar_wrapper(aux): # usado para passar um único parâmetro nos submits
+        id_thread = threading.get_ident()
+        checar(aux[0], id_thread, aux[1], erros, aux[2])
+
+    with ThreadPoolExecutor(max_workers=n_threads) as executor:
+        for i in range(9):
+            executor.submit(checar_wrapper, (tabuleiro[i], i, "L"))
+            executor.submit(checar_wrapper, (transposto[i], i, "C"))
+            executor.submit(checar_wrapper, (tab_em_blocos[i], i, "R"))
+    
+    num_erros = 0
+    valores = list(erros.values())
+    for v in valores:
+        num_erros += len(v)
+    
+    print(f'{num_erros} erros encontrados', end=" ") # imprime a qtd. de erros, as threads que encontraram, e as linhas/colunas/regiões onde há erro
+    if num_erros > 0:
+        texto = ""
+        for i in range(len(valores)):
+            if len(valores[i]) < 1:
+                continue
+            texto += f"T{i+1}: {', '.join(valores[i])}"
+        print(f"({texto})")
     else:
         print("")
 
--- sudoku.py
+++ sudoku.py
@@ -1,36 +1,5 @@
 TAMANHO_TABULEIRO = 9 # supõe-se que todos os tabuleiros são 9x9
 
-# def ler(arquivo):
-#     array_tabuleiros = []
-#     with open(arquivo, "r") as a:
-#         tabuleiro = []
-#         for line in a: # percorre todas as linhas do arquivo
-#             if line.strip == '': # se a linha for vazia, é o fim de um tabuleiro
-#                 if len(tabuleiro) != TAMANHO_TABULEIRO:
-#                     raise Exception("Tabuleiro inválido")
-#                 array_tabuleiros.append(tabuleiro.copy()) # adiciona o tabuleiro à lista de tabuleiros
-#                 tabuleiro = [] # remove os conteúdos para começar outro tabuleiro
-#             else:
-#                 linha_sudoku = []
-#                 for x in line:
-#                     try:
-#                         linha_sudoku.append(int(x))
-#                     except ValueError:
-#                         continue
-                
-#                 if len(linha_sudoku) != TAMANHO_TABULEIRO:
-#                     raise Exception("Número inválido de elementos")
-#                 elif any((x < 1 and x > TAMANHO_TABULEIRO) or type(x) is not int for x in linha_sudoku):
-#                     raise Exception("Caractere(s) inválido(s) no tabuleiro")
-#                 tabuleiro.append(linha_sudoku)
-
-#                 atual = a.tell()
-#                 proxima = next(a, 'end')
-#                 if proxima == 'end': # se for a última linha, adiciona o último tabuleiro à lista e retorna
-#                     array_tabuleiros.append(tabuleiro)
-#                     return array_tabuleiros
-#                 a.seek(atual)
-
 def ler(arquivo):
     a = open(arquivo, "r")
     linhas_texto = a.readlines()
@@ -65,6 +34,20 @@ def ler(arquivo):
                 array_tabuleiros.append(tabuleiro.copy())
                 return array_tabuleiros
 
+def checar_linha(linha, id_thread, pos, erros, letra):
+    if len(set(linha)) != TAMANHO_TABULEIRO: # se for false, há erro na linha
+        erros[id_thread].append(letra + str(pos)) # append em lista é atômico, segundo https://superfastpython.com/thread-safe-list/
+
+def transpor(tabuleiro):
+    return list(map(list, zip(*tabuleiro)))
+
+def obter_regioes_como_linhas(tabuleiro):
+    lista_regioes = []
+    for i in range(0, TAMANHO_TABULEIRO, 3):
+        for j in range(0, TAMANHO_TABULEIRO, 3):
+            regiao_em_linha = tabuleiro[i][j:j+3] + tabuleiro[i+1][j:j+3] + tabuleiro[i+2][j:j+3] # obtém a região em formato de linha
+            lista_regioes.append(regiao_em_linha)
+    return lista_regioes
 
 def checar_linhas(tabuleiro):
     lista_erros_linha = []
